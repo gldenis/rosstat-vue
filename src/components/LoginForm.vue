8a609fcbbe96a9943d85eaba010ea3dadcab5eab
Denis Glinkyi <gl.denis1996@gmail.com>
2024-02-09 15:58:04 +0200

    FEAT: add validation login form

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,18 +1,46 @@
 <script setup>
 import IconMe from '@/components/icons/IconMe.vue'
 
-const formSubmit = () => {
+import { useForm } from 'vee-validate'
+import { object, string } from "yup"
+import { reactive } from "vue"
 
+const schema = object({
+  email: string().required('email не может быть пустым').email("введите валидный email"),
+  password: string().required('пароль не может быть пустым'),
+})
+
+const { values, errors, defineField } = useForm({
+  validationSchema: schema,
+})
+
+const [email, emailAttrs] = defineField('email', {
+  validateOnModelUpdate: false,
+})
+
+const [password, passwordAttrs] = defineField('password', {
+  validateOnModelUpdate: false,
+})
+
+const formSubmit = async () => {
+  if (Object.keys(errors).length) alert("")
 }
 </script>
 
 <template>
-  <form class="auth-form" @click.prevent="formSubmit">
+  <form class="auth-form" @submit.prevent="formSubmit">
     <IconMe class="auth-form__icon" width="48" height="48" />
     <div class="auth-form__title">Войти</div>
+    {{ errors.length }}
     <div class="auth-form__fields-list">
-      <input type="text" class="form-field__input" placeholder="Адрес эл. почты">
-      <input type="password" class="form-field__input" placeholder="Пароль">
+      <div class="form-field">
+        <input type="text" class="form-field__input" placeholder="Адрес эл. почты" v-model="email" v-bind="emailAttrs">
+        <div class="form-field__error">{{ errors.email }}</div>
+      </div>
+      <div class="form-field">
+        <input type="password" class="form-field__input" placeholder="Пароль" v-model="password" v-bind="passwordAttrs">
+        <div class="form-field__error">{{ errors.password }}</div>
+      </div>
     </div>
     <div class="auth-form__buttons">
       <button class="btn btn--primary">Войти</button>
